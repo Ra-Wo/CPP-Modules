c50419f37d2d5088a09a255a816c87b83d10ee4c
Rachid oudouch <rashidoudouch@gmail.com>
2022-08-29 17:13:47 +0100

    add ex00

--- Module 07/ex00/Header.hpp
+++ Module 07/ex00/Header.hpp
@@ -1,23 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Header.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: roudouch <roudouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/29 14:38:01 by roudouch          #+#    #+#             */
-/*   Updated: 2022/08/29 17:00:54 by roudouch         ###   ########.fr       */
+/*   Created: 2022/08/29 16:57:02 by roudouch          #+#    #+#             */
+/*   Updated: 2022/08/29 17:09:36 by roudouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-int main() {
+#ifndef HEADER_HPP
+#define HEADER_HPP
+#include <iostream>
 
-    Base *ptr = generate();
-    
-    identify(*ptr);
-    identify(ptr);
-
-    delete ptr;
-    return 0;
+template<typename T> void swap(T& var1, T&var2) {
+    T backup = var1;
+    var1 = var2;
+    var2 = backup;
 }
+
+template<typename T> T &min(T& var1, T&var2) {
+    return (
+        (var2 <= var1) ? var2 : var1
+    ); 
+}
+
+template<typename T> T &max(T& var1, T&var2) {
+    return (
+        (var2 >= var1) ? var2 : var1
+    ); 
+}
+
+#endif
--- Module 07/ex00/main.cpp
+++ Module 07/ex00/main.cpp
@@ -5,19 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: roudouch <roudouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/29 14:38:01 by roudouch          #+#    #+#             */
-/*   Updated: 2022/08/29 17:00:54 by roudouch         ###   ########.fr       */
+/*   Created: 2022/08/29 16:57:08 by roudouch          #+#    #+#             */
+/*   Updated: 2022/08/29 17:10:28 by roudouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Header.hpp"
 
-int main() {
-
-    Base *ptr = generate();
-    
-    identify(*ptr);
-    identify(ptr);
-
-    delete ptr;
+int main( void ) {
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
     return 0;
 }
