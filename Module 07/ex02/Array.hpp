0e56f6470183f15d72344c1b0b8c9ac1e85096f4
Rachid oudouch <rashidoudouch@gmail.com>
2022-09-01 17:55:36 +0100

    ex02

---
+++ Module 07/ex02/Array.hpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roudouch <roudouch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 15:56:09 by roudouch          #+#    #+#             */
+/*   Updated: 2022/09/01 17:53:58 by roudouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+#include <iostream>
+    
+template<typename T> class Array {
+
+    private:
+        T* array;
+        size_t size;
+
+    public:
+        Array() {
+            this->array = nullptr;
+            this->size = 0;
+        }
+        
+        Array(size_t n) {
+            this->size = n;
+            if (n < 1) this->array = nullptr;
+            else this->array = new T[n];
+        }
+        
+        Array(Array &src) {
+            *this = src;
+        }
+        
+        ~Array() {}
+        
+        // operators
+        Array &operator=(Array &src) {
+            if (this != &src) {
+                delete this->array;
+                this->array = new T[src.getSize()];
+                for (size_t i = 0; i < src.getSize(); i++) {
+                   this->array[i] = src.array[i];
+                }
+            }
+            return *this;
+        }
+
+        T &operator[](size_t index) {
+            if (index < 0 || index >= this->getSize()) {
+                throw OutOfRange();
+            };
+            return this->getArray()[index];
+        }
+
+        // exception
+        class OutOfRange : public std::exception {
+            const char *what() const throw() {
+                return "The element you are tring to access is not exist!";
+            }
+        };
+
+        //getters
+        T* getArray() {
+            return this->array;
+        }
+        const size_t &getSize() const {
+            return this->size;
+        }
+};
+
+#endif
---
+++ Module 07/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roudouch <roudouch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 15:56:29 by roudouch          #+#    #+#             */
+/*   Updated: 2022/09/01 17:54:45 by roudouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+
+int main() {
+
+    Array<int> *ptr = new Array<int>(5);
+
+    try {
+        
+        std::cout << (*ptr)[2] << '\n';
+        
+    } catch (std::exception &error) {
+        std::cout << error.what() << '\n';
+    }
+
+    delete ptr;
+    
+    return 0;
+}
