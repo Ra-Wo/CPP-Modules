a48fe69df483730073a7bb14c944ecebfcb1f19f
Rachid oudouch <rashidoudouch@gmail.com>
2022-09-01 15:48:30 +0100

    ex01

--- Module 07/ex01/iter.hpp
+++ Module 07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: roudouch <roudouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 17:16:19 by roudouch          #+#    #+#             */
-/*   Updated: 2022/08/29 17:16:42 by roudouch         ###   ########.fr       */
+/*   Updated: 2022/09/01 15:47:48 by roudouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,17 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 #include <iostream>
+    
+template<typename T>
+    void iter(T* array, int length, void (*func)(T &element)) {
+        for (int i = 0; i < length; i++) {
+            func(array[i]);
+        }
+    }
 
-
+template<typename T>
+    void funcToRun(T &e) {
+        e++;
+    }
 
 #endif
--- Module 07/ex01/main.cpp
+++ Module 07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: roudouch <roudouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 17:15:58 by roudouch          #+#    #+#             */
-/*   Updated: 2022/08/29 17:16:17 by roudouch         ###   ########.fr       */
+/*   Updated: 2022/09/01 15:33:05 by roudouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,32 @@
 int main() {
 
 
-
+    int arr[] = {0, 8, 8, 9, 6, 3, 22};
+    char arr1[] = "hello ?";
+    int len = 7;
+    
+    std::cout << "\n*********** before ***********\n\n";
+    
+    for (int i = 0; i < 7; i++) {
+        std::cout << arr[i] << (i < 6? " - " : "\n");
+    }
+    
+    for (int i = 0; i < 7; i++) {
+        std::cout << arr1[i] << (i < 6? " - " : "\n");
+    }
+    
+    std::cout << "\n********* after **************\n\n";
+    
+    iter(arr, len, &funcToRun);
+    for (int i = 0; i < 7; i++) {
+        std::cout << arr[i] << (i < 6? " - " : "\n");
+    }
+    
+    iter(arr1, len, &funcToRun);
+    for (int i = 0; i < 7; i++) {
+        std::cout << arr1[i] << (i < 6? " - " : "\n");
+    }
+    std::cout << "\n********** end ***************\n\n";
+    
     return 0;
 }
