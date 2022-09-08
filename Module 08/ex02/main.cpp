2d7f65fd3798e5f782979e072f2bf016cd0b6ce5
Rachid oudouch <rashidoudouch@gmail.com>
2022-09-08 15:59:32 +0100

    add test

--- Module 08/ex02/main.cpp
+++ Module 08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: roudouch <roudouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 16:01:26 by roudouch          #+#    #+#             */
-/*   Updated: 2022/09/05 17:25:01 by roudouch         ###   ########.fr       */
+/*   Updated: 2022/09/08 15:58:43 by roudouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int main() {
     return 0;
 }
 
-
+//#include <list>
 //int main()
 //{
 //    MutantStack<int> mstack;
@@ -64,6 +64,38 @@ int main() {
 //        ++it;
 //    }
 //    std::stack<int> s(mstack);
+
+//    std::cout << "\n@@@@@@@@@@@@@@@@@@@@@@@@\n\n";
+
+//    std::list<int> lll;
+    
+//    lll.push_back(5);
+//    lll.push_back(17);
+    
+//    std::cout << lll.back() << std::endl;
+    
+//    lll.pop_back();
+    
+//    std::cout << lll.size() << std::endl;
+
+//    lll.push_back(3);
+//    lll.push_back(5);
+//    lll.push_back(737);
+//    lll.push_back(0);
+    
+//    std::list<int>::iterator list_it = lll.begin();
+//    std::list<int>::iterator list_ite = lll.end();
+    
+//    ++list_it;
+//    --list_it;
+//    while (list_it != list_ite)
+//    {
+//        std::cout << *list_it << std::endl;
+//        ++list_it;
+//    }
+//    std::list<int> list_s(lll);
+
+    
 //    return 0;
 //}
 
